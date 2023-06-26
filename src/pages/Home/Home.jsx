f25bc187de8ce92bdcd64e7806e369896be230e5
amenigrami <AmeniGrami@takiacademyteam.com>
2023-06-26 09:22:35 +0100

    initial commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,96 +1,12 @@
 import "./Home.css";
 import banner from "../../assets/icons/Banner.svg";
 import cart from "../../assets/cart.svg";
-import almonds from "../../assets/almonds.png";
-import broc from "../../assets/broc.png";
+
 import rating from "../../assets/rating.svg";
 
-import banana from "../../assets/banana.png";
-import nuts from "../../assets/nuts.png";
-import tomato from "../../assets/tomato.png";
-import mung from "../../assets/Mung.png";
-import hazelnut from "../../assets/hazel.png";
-import eggs from "../../assets/eggs.png";
-import rusk from "../../assets/rusk.png";
-import bean from "../../assets/bean.png";
-import white from "../../assets/whazel.png";
-import corn from "../../assets/corn.png";
+import { Products } from "../../utils/constants";
 function Home() {
-  const Products =[
-    {tag:"Vegetable",
-     img:broc,
-     name:"Calabrese Broccoli",
-     oldPrice:"$20.00",
-     newPrice:"$13.00"
-  },
-  {tag:"Fresh",
-     img:banana,
-     name:"Fresh Banana Fruites",
-     oldPrice:"$20.00",
-     newPrice:"$14.00"
-  },
-  {tag:"Millets",
-     img:nuts,
-     name:"White Nuts",
-     oldPrice:"$20.00",
-     newPrice:"$15.00"
-  },
-  {tag:"Vegetable",
-     img:tomato,
-     name:"Vegan Red Tomato",
-     oldPrice:"$200",
-     newPrice:"$17.00"
-  },
-  {tag:"Health",
-     img:mung,
-     name:"Mung Bean",
-     oldPrice:"$20.00",
-     newPrice:"$11.00"
-  },
-  {tag:"Nuts",
-     img:hazelnut,
-     name:"Brown Hazelnut",
-     oldPrice:"$20.00",
-     newPrice:"$12.00"
-  },
-  {tag:"Fresh",
-     img:eggs,
-     name:"Eggs",
-     oldPrice:"$20.00",
-     newPrice:"$17.00"
-  },
-  {tag:"Fresh",
-     img:rusk,
-     name:"Zelco Suji Elaichi Rusk",
-     oldPrice:"$20.00",
-     newPrice:"$15.00"
-  },
-  {tag:"Health",
-  img:bean,
-  name:"Mung Bean",
-  oldPrice:"$20.00",
-  newPrice:"$11.00"
-},
-{tag:"Nuts",
-img:white,
-name:"White Hazelnut",
-oldPrice:"$20.00",
-newPrice:"$12.00"
-},
-{tag:"Fresh",
-img:corn,
-name:"Fresh Corn",
-oldPrice:"$20.00",
-newPrice:"$17.00"
-},
-{tag:"Fresh",
-img:almonds,
-name:"Organic Almonds",
-oldPrice:"$20.00",
-newPrice:"$15.00"
-},
-
-  ]
+  
   return (
     <div className="home_page">
       <img className="banner_img" src={banner} alt="" />
---
+++ src/utils/constants.js
@@ -0,0 +1,87 @@
+import banana from "../assets/banana.png";
+import nuts from "../assets/nuts.png";
+import tomato from "../assets/tomato.png";
+import mung from "../assets/Mung.png";
+import hazelnut from "../assets/hazel.png";
+import eggs from "../assets/eggs.png";
+import rusk from "../assets/rusk.png";
+import bean from "../assets/bean.png";
+import white from "../assets/whazel.png";
+import corn from "../assets/corn.png";
+import almonds from "../assets/almonds.png";
+import broc from "../assets/broc.png";
+export const Products = [
+    {tag:"Vegetable",
+     img:broc,
+     name:"Calabrese Broccoli",
+     oldPrice:"$20.00",
+     newPrice:"$13.00"
+  },
+  {tag:"Fresh",
+     img:banana,
+     name:"Fresh Banana Fruites",
+     oldPrice:"$20.00",
+     newPrice:"$14.00"
+  },
+  {tag:"Millets",
+     img:nuts,
+     name:"White Nuts",
+     oldPrice:"$20.00",
+     newPrice:"$15.00"
+  },
+  {tag:"Vegetable",
+     img:tomato,
+     name:"Vegan Red Tomato",
+     oldPrice:"$200",
+     newPrice:"$17.00"
+  },
+  {tag:"Health",
+     img:mung,
+     name:"Mung Bean",
+     oldPrice:"$20.00",
+     newPrice:"$11.00"
+  },
+  {tag:"Nuts",
+     img:hazelnut,
+     name:"Brown Hazelnut",
+     oldPrice:"$20.00",
+     newPrice:"$12.00"
+  },
+  {tag:"Fresh",
+     img:eggs,
+     name:"Eggs",
+     oldPrice:"$20.00",
+     newPrice:"$17.00"
+  },
+  {tag:"Fresh",
+     img:rusk,
+     name:"Zelco Suji Elaichi Rusk",
+     oldPrice:"$20.00",
+     newPrice:"$15.00"
+  },
+  {tag:"Health",
+  img:bean,
+  name:"Mung Bean",
+  oldPrice:"$20.00",
+  newPrice:"$11.00"
+},
+{tag:"Nuts",
+img:white,
+name:"White Hazelnut",
+oldPrice:"$20.00",
+newPrice:"$12.00"
+},
+{tag:"Fresh",
+img:corn,
+name:"Fresh Corn",
+oldPrice:"$20.00",
+newPrice:"$17.00"
+},
+{tag:"Fresh",
+img:almonds,
+name:"Organic Almonds",
+oldPrice:"$20.00",
+newPrice:"$15.00"
+},
+
+  ]
