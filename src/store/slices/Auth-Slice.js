e3a4af717d9e44ea265a632adf11911fccc27680
Marwen.eshili <marweneshili@takiacademyteam.com>
2023-07-11 13:45:29 +0100

    add register async thunk function

--- src/store/slices/Auth-Slice.js
+++ src/store/slices/Auth-Slice.js
@@ -1,10 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (values, thunkAPI) => {
+    try {
+      const response = await axios.fetchById("url here", values);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+      return Promise.reject(error.message);
+    }
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {},
   reducers: {},
 });
+
 export const {} = authSlice.actions;
 
 export default authSlice.reducer;
