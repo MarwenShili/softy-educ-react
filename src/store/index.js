a72a557c3902f1366c38001d290c97fc150cc7dc
Marwen.eshili <marweneshili@takiacademyteam.com>
2023-07-11 13:34:27 +0100

    create auth slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/Cart-Slice";
+import authReducer from "./slices/Auth-Slice";
 
 const store = configureStore({
-  reducer: { cart: cartReducer },
+  reducer: { cart: cartReducer, auth: authReducer },
 });
 export default store;
